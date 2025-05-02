89b020fa7298da50632381af7a1324475aa91912
Victoria-cyb <chiamakamichael58@gmail.com>
2025-05-02 11:19:45 +0100

    intial commit

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
 
 const app = express();
 
-app.use(cors())
+app.use(cors({ origin: 'http://127.0.0.1:5500' }))
 
 const resolvers = {
   Query: {
